8037b41c70d872a183d8e07f704b44ee9beae4ba
Akelsh <ahmad@elshiltawi.com>
2025-09-15 13:42:54 +0300

    Initial Commit

---
+++ Seek/SeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekApp.swift
+//  Seek
+//
+//  Created by Ahmad on 2025-09-15.
+//
+
+import SwiftUI
+
+@main
+struct SeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
